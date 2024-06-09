e7911651b0febb5532d515690b6cd3732fc89867
Derry Alantis <alantisderry@gmail.com>
2024-06-09 11:42:23 +0700

    setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+
+const formula = localFont({
+  src: [
+    {
+      path: "./fonts/formula1/Formula1-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/formula1/Formula1-Bold.otf",
+      weight: "700",
+      style: "bold",
+    },
+    {
+      path: "./fonts/formula1/Formula1-Wide.otf",
+      weight: "900",
+      style: "wide",
+    },
+  ],
+  variable: "--font-formula1",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${formula.variable} font-formula`}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#e10600",
+        secondary: "#1f1f27",
+        tertiary: "#b1b1b3",
+        shade1: "#b40500",
+        shade2: "#9e0400",
+        shade3: "#870400",
+        shade4: "#710300",
+        shade5: "#5a0200",
+      },
+      fontFamily: {
+        formula: ["var(--font-formula1)"],
+      },
     },
   },
   plugins: [],
